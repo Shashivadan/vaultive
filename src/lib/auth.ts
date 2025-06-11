cff4fbbe781bd4d81b883b8ec05e54294bcff792
Shashivadan <shashivadan99@gmail.com>
2025-06-11 22:47:34 +0530

    feat: added better-auth"

---
+++ src/lib/auth.ts
@@ -0,0 +1,3 @@
+import { db } from "@/server/jstack";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
--- src/server/jstack.ts
+++ src/server/jstack.ts
@@ -1,31 +1,58 @@
-import { jstack } from "jstack"
-import { drizzle } from "drizzle-orm/postgres-js"
-import { env } from "hono/adapter"
+import { InferMiddlewareOutput, jstack } from "jstack";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { env } from "hono/adapter";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { betterAuth } from "better-auth";
+import { HTTPException } from "hono/http-exception";
 
 interface Env {
-  Bindings: { DATABASE_URL: string }
+  Bindings: { DATABASE_URL: string };
 }
 
-export const j = jstack.init<Env>()
+export const j = jstack.init<Env>();
 
 /**
  * Type-safely injects database into all procedures
  * @see https://jstack.app/docs/backend/middleware
- * 
+ *
  * For deployment to Cloudflare Workers
  * @see https://developers.cloudflare.com/workers/tutorials/postgres/
  */
+
 const databaseMiddleware = j.middleware(async ({ c, next }) => {
-  const { DATABASE_URL } = env(c)
+  const { DATABASE_URL } = env(c);
+  const db = drizzle(DATABASE_URL);
+  return await next({ db });
+});
 
-  const db = drizzle(DATABASE_URL)
+const authMiddleware = j.middleware(async ({ c, ctx, next }) => {
+  const { db } = ctx as InferMiddlewareOutput<typeof databaseMiddleware>;
+  const auth = betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg", // or "mysql", "sqlite"
+    }),
+  });
+  return await next({ auth });
+});
 
-  return await next({ db })
-})
+const authenticationMiddleware = j.middleware(async ({ c, ctx, next }) => {
+  const { auth } = ctx as InferMiddlewareOutput<typeof authMiddleware>;
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    throw new HTTPException(401, {
+      message: "Unauthenticated, sign in to continue.",
+    });
+  }
+  return await next({ session });
+});
 
 /**
  * Public (unauthenticated) procedures
  *
  * This is the base piece you use to build new queries and mutations on your API.
  */
-export const publicProcedure = j.procedure.use(databaseMiddleware)
+export const publicProcedure = j.procedure
+  .use(databaseMiddleware)
+  .use(authMiddleware);
+
+export const protectedProcedure = publicProcedure.use(authenticationMiddleware);
